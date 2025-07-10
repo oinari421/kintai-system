37c2f9444b39e1f9dcdfeb3a5bc34785f401f90b
oinari421 <kaitoangle@gmail.com>
2025-07-10 10:45:54 +0900

    feat: 最初のユーザーに管理者権限を付与する処理を追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,23 +1,20 @@
 <?php
-
-namespace Database\Seeders;
-
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (User::count() === 0) {
+            // 最初のユーザーを作成し、is_admin を true にする
+            User::create([
+                'name' => 'Admin User',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('password'),
+                'is_admin' => true,
+            ]);
+        }
     }
 }
