263c1c5560013fb66880b8534b1d7db41258faac
oinari421 <kaitoangle@gmail.com>
2025-07-13 08:02:11 +0900

    fix: assign is_admin to first user only

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,21 +4,21 @@
 
         名前
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-label for="name" :value="名前を入力してください" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="メールアドレス" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         メールアドレス
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-label for="email" :value="メールアドレスを入力してください" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="メールアドレス" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         パスワード
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="パスワードを入力してください')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -30,7 +30,7 @@
 
         パスワード確認
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="パスワードを入力してください" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
